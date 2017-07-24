6aa503534f9c2d3cf278601b8cc0307a32d0e8e0
Santiago Basulto <santiago.basulto@gmail.com>
2017-07-24 12:33:35 -0400

    More lessons

---
+++ unit-1-getting-started/lesson-14-is-divisible-by/main.py
@@ -0,0 +1,20 @@
+divisible_by_11 = 11
+
+is_583_divisible_by_11 = None
+is_911_divisible_by_11 = None
+
+number_583 = 583
+
+if [YOUR_CODE]:  # Complete your code here
+    is_583_divisible_by_11 = True
+else:
+    is_583_divisible_by_11 = False
+
+
+is_911_divisible_by_11 = None
+number_911 = 911
+
+if [YOUR_CODE]:  # Complete your code here
+    is_911_divisible_by_11 = True
+else:
+    is_911_divisible_by_11 = False
